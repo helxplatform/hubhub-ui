651ae75e3b6db19cf8799ad45fa082cfd1e22095
Matt Watson <mwatson@renci.org>
2022-06-27 19:46:13 -0400

    install mui dependencies

--- src/views/deployments.js
+++
@@ -1,7 +0,0 @@
-export const DeploymentsView = () => {
-  return (
-    <p>
-      Fugiat ut dolor nostrud ullamco sed deserunt officia fugiat minim ex aliqua labore.
-    </p>
-  )
-}
--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,1 @@
-export * from './deployments'
-export * from './home'
 export * from './repos'
