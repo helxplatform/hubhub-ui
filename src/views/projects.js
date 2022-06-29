9bd6db3ad6170e4bdbbd05d880d29eeb0b6502d8
Matt Watson <mwatson@renci.org>
2022-06-29 19:13:43 -0400

    add latest tag column

--- src/views/projects.js
+++ src/views/projects.js
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { Box } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import { useApp } from '../context'
@@ -9,14 +9,23 @@ const columns = [
     headerName: 'Repository',
     width: 300,
   },
+  {
+    field: 'tags',
+    headerName: 'Latest Tag',
+    width: 300,
+  }
 ]
 
 export const ProjectsView = () => {
   const { projects, setCurrentProjectID } = useApp()
+  const [pageSize, setPageSize] = useState(25)
 
   const tableData = useMemo(() => projects
     ? Object.keys(projects)
-      .map(key => ({ id: projects[key].repository_name }))
+      .map(key => ({
+        id: projects[key].repository_name,
+        tags: Object.keys(projects[key].tags)[0],
+      }))
     : [], [projects])
 
   const handleClickRow = data => {
@@ -24,11 +33,12 @@ export const ProjectsView = () => {
   }
 
   return (
-    <Box sx={{ height: 800, width: '100%' }}>
+    <Box sx={{ height: 1200, width: '100%' }}>
       <DataGrid
         rows={ tableData }
         columns={ columns }
         onRowClick={ handleClickRow }
+        autoPageSize
       />
     </Box>
   )
