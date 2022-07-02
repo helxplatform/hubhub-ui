012c1f29e5c3f72b359a85328b653e05303f9bd0
Matt Watson <mwatson@renci.org>
2022-07-01 23:28:43 -0400

    add expand/collapse all buttons

--- src/components/project-drawer.js
+++ src/components/project-drawer.js
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import PropTypes from 'prop-types'
 import {
   Accordion, AccordionSummary, AccordionDetails,
@@ -8,14 +8,43 @@ import {
 import {
   Close as CloseIcon,
   ExpandMore as ExpandIcon,
+  UnfoldMore as ExpandAllIcon,
+  UnfoldLess as CollapseAllIcon,
 } from '@mui/icons-material'
 import { useApp } from '../context'
 
 export const ProjectDrawer = ({ open }) => {
   const { projects, closeDrawer, currentProjectID } = useApp()
+  const [expandedPanels, setExpandedPanels] = useState(new Set())
   const smallScreen = useMediaQuery(`(max-width: 600px)`)
+
+  const allExpanded = useMemo(() => {
+    if (!project) { return false }
+    return Object.keys(project.tags).some((tag, i) => expandedPanels.has(i))
+  }, [project, expandedPanels])
   
   const project = useMemo(() => projects[currentProjectID], [currentProjectID])
+
+  const handleClickPanel = key => {
+    let _expandedPanels = new Set(expandedPanels)
+    if (_expandedPanels.has(key)) {
+      _expandedPanels.delete(key)
+    } else {
+      _expandedPanels.add(key)
+    }
+    setExpandedPanels(_expandedPanels)
+  }
+
+  const handleClickCollapseAll = () => {
+    setExpandedPanels(new Set())
+  }
+  const handleClickExpandAll = () => {
+    const n = Object.keys(project.tags).length
+    const newExpandedPanels = new Set([...Array(n).keys()])
+    setExpandedPanels(newExpandedPanels)
+  }
+
+  console.log(expandedPanels, allExpanded)
   
   return (
     <Drawer
@@ -41,7 +70,7 @@ export const ProjectDrawer = ({ open }) => {
         <Stack
           direction="row"
           alignItems="stretch"
-          sx={{ height: '100%' }}
+          sx={{ height: '100%', width: '100%' }}
         >
           <IconButton
             onClick={ closeDrawer }
@@ -50,9 +79,23 @@ export const ProjectDrawer = ({ open }) => {
             <CloseIcon />
           </IconButton>
           <Divider orientation="vertical" />
-          <Typography variant="h4" sx={{ display: 'flex', alignItems: 'center', padding: '0 1rem' }}>
+          <Typography variant="h4" sx={{ flex: 1, display: 'flex', alignItems: 'center', padding: '0 1rem' }}>
             { project?.repository_name || '...' }
           </Typography>
+          <Divider orientation="vertical" />
+          <IconButton
+            onClick={ handleClickCollapseAll }
+            sx={{ borderRadius: 0, height: '100%', width: '32px' }}
+          >
+            <CollapseAllIcon />
+          </IconButton>
+          <Divider orientation="vertical" />
+          <IconButton
+            onClick={ handleClickExpandAll }
+            sx={{ borderRadius: 0, height: '100%', width: '32px' }}
+          >
+            <ExpandAllIcon />
+          </IconButton>
         </Stack>
       </Toolbar>
       {
@@ -63,9 +106,11 @@ export const ProjectDrawer = ({ open }) => {
                 const tag = project.tags[key]
                 return (
                   <Accordion
+                    onChange={ () => handleClickPanel(i) }
                     key={ `${ project.repository_name }-${ tag.tag_name }` }
                     elevation={ 0 }
                     defaultExpanded={ i === 0 }
+                    expanded={ expandedPanels.has(i) }
                   >
                     <AccordionSummary
                       expandIcon={ <ExpandIcon /> }
